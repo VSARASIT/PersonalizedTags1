d2b285d92045ad52252809bf64ac010d8024b353
Manish <manishkumar2104@gmail.com>
2023-10-23 00:29:31 +0530

    Updated code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageEighteenComponent } from './page-eighteen/page-eighteen.component';
+import { PageSixToEightComponent } from './page-six-to-eight/page-six-to-eight.component';
+import { PageTwentySevenComponent } from './page-twenty-seven/page-twenty-seven.component';
+import { PageTwentySixComponent } from './page-twenty-six/page-twenty-six.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'page-eighteen', component: PageEighteenComponent },
+  { path: 'page-six-to-eight', component: PageSixToEightComponent },
+  { path: 'page-twenty-seven', component: PageTwentySevenComponent },
+  { path: 'page-twenty-six', component: PageTwentySixComponent },
+  // Add more routes as needed
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PageSixToEightComponent } from './page-six-to-eight/page-six-to-eight.component';
+import { PageEighteenComponent } from './page-eighteen/page-eighteen.component';
+import { PageTwentySixComponent } from './page-twenty-six/page-twenty-six.component';
+import { PageTwentySevenComponent } from './page-twenty-seven/page-twenty-seven.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
+    PageSixToEightComponent,
+    PageEighteenComponent,
+    PageTwentySixComponent,
+    PageTwentySevenComponent,
   ],
   imports: [
     BrowserModule,

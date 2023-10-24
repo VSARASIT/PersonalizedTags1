c7b03887d360bd0e6e0cc3c8c6d63512f1fc4e4d
Manish <manishkumar2104@gmail.com>
2023-10-25 00:23:08 +0530

    added pop up

--- src/app/page-eighteen/page-eighteen.component.ts
+++ src/app/page-eighteen/page-eighteen.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-page-eighteen',
   templateUrl: './page-eighteen.component.html',
   styleUrls: ['./page-eighteen.component.scss']
+  
 })
-export class PageEighteenComponent {
+export class PageEighteenComponent { 
+	 
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+		// customize default values of modals used by this component tree
+		config.backdrop = 'static';
+		config.keyboard = false;
+	}
 
+	open(content: any) {
+		this.modalService.open(content);
+	}
 }
--- src/app/page-six-to-eight/page-six-to-eight.component.ts
+++ src/app/page-six-to-eight/page-six-to-eight.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbOffcanvas} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-page-six-to-eight',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-six-to-eight.component.scss']
 })
 export class PageSixToEightComponent {
+  
+  closeResult = '';
 
+	constructor(private offcanvasService: NgbOffcanvas) {}
+
+	open(content: any) {
+		this.offcanvasService.open(content, { ariaLabelledBy: 'offcanvas-basic-title' }).result.then(
+			(result) => {
+				this.closeResult = `Closed with: ${result}`;
+			},		 
+		);
+	}
+ 
 }
